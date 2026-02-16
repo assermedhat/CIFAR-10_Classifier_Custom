496be5b06b0303b9f4eeb38cea347da9136b1d49
assermedhat <assermedhat001@gmail.com>
2026-02-17 01:04:25 +0200

    ResNet+SGD

--- CIFAR_ResNet.py
+++ CIFAR_ResNet.py
@@ -64,7 +64,15 @@ class Initialize_ResNet:
 
 class Main(Orchestrator):
     def __init__(self,writer,train_dataloader,test_dataloader,model,lr=0.001,l2_reg=0.0,retrain=False,checkpoint="resnet_cifar10.pth"):
-        Orchestrator.__init__(self,writer,train_dataloader,test_dataloader,model=model,lr=lr,l2_reg=l2_reg,retrain=retrain,checkpoint_path=checkpoint)
+        super().__init__(writer,train_dataloader,test_dataloader,model=model,lr=lr,l2_reg=l2_reg,retrain=retrain,checkpoint_path=checkpoint)
+        
+        self.opt=torch.optim.SGD(model.parameters(),lr=lr,momentum=0.9,weight_decay=l2_reg)
+        
+        self.scheduler=torch.optim.lr_scheduler.CosineAnnealingLR(self.opt,T_max=30)
+    def train_one_epoch(self):
+        loss,acc=super().train_one_epoch()
+        self.scheduler.step()
+        return loss,acc
 
 
 
@@ -73,8 +81,8 @@ if __name__ == "__main__":
     model = ResNet18().to(device)
     data=Initialize_ResNet()
     train_data,test_data,writer=data.start()
-    main=Main(writer,train_data,test_data,model=model,lr=0.0001,retrain=True,l2_reg=0.0005)
-    main.fit(epochs=10)
+    main=Main(writer,train_data,test_data,model=model,lr=0.1,retrain=False,l2_reg=0.0005)
+    main.fit(epochs=30)
     test_loss,test_acc=main.evaluate()
     print(f"Final test loss = {test_loss:.4f}\nFinal test accuracy = {test_acc*100:.2f}")
     # summary(model,input_size=(64,3,32,32))
